ff42caf448e063358b7a0d6625fa8bfafde74192
Henry <zhiwei.yang1997@gmail.com>
2019-10-05 16:48:10 -0500

    update the mean finder without given count

--- meanFinder.java
+++ meanFinder.java
@@ -1,15 +1,20 @@
-
 public class meanFinder
 {
-    private int mean;
-    private int sum;
     private int count;
-    
-    private int[] data;
 
-    public meanFinder()
+    public meanFinder(int number[])
     {
-
+		if(number.length() == 0)
+			return -1;
+		int sum = 0;
+		for(int i = 0; i < number.length(); i++)
+			sum = sum + number[i];
+		return sum/number.length();
     }
-
+	
+	public static void main(String[] args)
+	{
+		int[] data = [2, 3, 9, 5, 48, 6, 4, 6, 15, 26, 38, 75, 56];
+		println(meanFinder(data))
+	}
 }
