4f12c7173eedce30ab16f137be69c7aaea5b7d71
Evan Krug <kruge@uwplatt.edu>
2019-10-05 19:16:51 -0500

    fixing merge conflict

--- meanFinder.java
+++ meanFinder.java
@@ -2,11 +2,11 @@ public class meanFinder
 {
     private int count;
 
-<<<<<<< HEAD
+
     public void meanFinder()
-=======
+
     public meanFinder(int number[])
->>>>>>> 897ee92e926023fd5770e99d03102b25aa5b68aa
+
     {
 		if(number.length() == 0)
 			return -1;
@@ -15,20 +15,12 @@ public class meanFinder
 			sum = sum + number[i];
 		return sum/number.length();
     }
-<<<<<<< HEAD
-    
 
-
-
-
-
-
-=======
 	
 	public static void main(String[] args)
 	{
 		int[] data = [2, 3, 9, 5, 48, 6, 4, 6, 15, 26, 38, 75, 56];
 		println(meanFinder(data))
 	}
->>>>>>> 897ee92e926023fd5770e99d03102b25aa5b68aa
+
 }
